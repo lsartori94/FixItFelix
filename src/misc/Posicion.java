d2a32fd227db51a3220afe73e87d5f4c6f059c92
lsartori <lucasartori94@gmail.com>
2015-10-12 15:48:55 -0300

    Agregado pkg misc y clase posicion.

---
+++ src/misc/Posicion.java
@@ -0,0 +1,19 @@
+package misc;
+
+public class Posicion {
+	private int x;	//posicion horizontal
+	private int y;	//posicion vertical
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+}
