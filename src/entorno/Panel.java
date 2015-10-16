f6d9ca5bf24cbb0e556a3a367045615d1b13074e
lsartori <lucasartori94@gmail.com>
2015-10-15 23:10:41 -0300

    Clase Panel comentada

--- src/entorno/Panel.java
+++ src/entorno/Panel.java
@@ -1,9 +1,20 @@
 package entorno;
-
+/**
+ * Clase que simula el Panel de una Ventana.
+ * Puede estar parcial o completamente roto.
+ * @author lsartori
+ */
 public class Panel {
 	private boolean parcial;
 	private boolean completa;
 	
+	/**
+	 * Constructor del Panel
+	 * Si ambos parametros estan en false, el panel esta sano.
+	 * 
+	 * @param parcial = indica si el panel esta parcialmente roto.
+	 * @param completa = indica si el panel esta completamente roto.
+	 */
 	public Panel(boolean parcial, boolean completa){
 		setParcial(parcial);
 		setCompleta(completa);
@@ -25,12 +36,21 @@ public class Panel {
 		this.completa = completa;
 	}
 
+	/**
+	 * Arregla el panel.
+	 * Si el panel esta completamente roto lo pone en parcial.
+	 * Si el panel esta parcialmeente roto lo arregla.
+	 */
 	public void arreglar(){
 		if(parcial())
 			setCompleta(true);
 		setParcial(true);
 	}
 
+	/**
+	 * Rompe el panel.
+	 * Pone el panel completamente roto.
+	 */
 	public void romper(){
 		setParcial(false);
 		setCompleta(false);
